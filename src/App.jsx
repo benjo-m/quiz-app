4920994e484355e487a38e5c97204f5520442dbe
benjo-m <maglicbenjamin@gmail.com>
2023-04-13 18:55:44 +0200

    added question component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import StartScreen from './components/StartScreen'
 import Header from './components/Header'
 import './style.css'
@@ -8,23 +8,46 @@ import Question from './components/Question'
 function App() {
   const [questions, setQuestions] = useState([])
   const [gameStarted, setGameStarted] = useState(false)
+  const [questionNumber, setQuestionNumber] = useState(0)
+  const [selectedAnswers, setSelectedAnswers] = useState([])
 
   function fetchData(numberOfQuestions, category, difficulty) {
     fetch(`https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&difficulty=${difficulty}`)
       .then(res => res.json())
       .then(data => {
-        setQuestions(data.results[0].incorrect_answers)
+        setQuestions(data.results)
       })
-      setGameStarted(true)
+    setGameStarted(true)
   }
 
-  console.log(questions)
+  let qArr = []
+  function createQuestionCards() {
+    let questionsArr = questions.map(q => {
+      return <Question question={q.question} incorrectAnswers={q.incorrect_answers} 
+        correctAnswer={q.correct_answer} handleClick={next} selectAnswer={selectAnswer}/>
+    })
+    return questionsArr
+  }
+
+  qArr = createQuestionCards()
+
+  // console.log(questions)
+
+  function next() {
+    setQuestionNumber(prev => prev + 1)
+  }
+
+  function selectAnswer(event) {
+    console.log(selectedAnswers)
+    setSelectedAnswers(prev => [...prev, event.target.textContent])
+  }
 
   return (
     <div id="main-div">
       <Header />
-      {gameStarted && <Question answers={questions}/>}
-      {!gameStarted && <StartScreen handleFetch={fetchData}/>}
+      {gameStarted &&
+        <div className="questions-div">{qArr[questionNumber]}</div>}
+      {!gameStarted && <StartScreen handleFetch={fetchData} />}
     </div>
   )
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,16 +1,23 @@
 export default function Question(props) {
-    let ans = props.answers.map(answer => {
-        return(
-            <div key={answer}>
-                <label key={`${answer}1`} htmlFor={answer}>{answer}</label>
-                <input key={answer} type="radio" name={answer} id={answer} />
-            </div>
-        )
+    // const [selectedAnswers, setSelectedAnswers] = useState([])
+
+    // function selectAnswer(event) {
+    //     setSelectedAnswers(prev => [...prev, event.target.textContent])
+    // }
+
+    let incorrectAnswers = props.incorrectAnswers.map(a => {
+        return <div className="answer" key={a} onClick={event => props.selectAnswer(event)}>{a}</div>
     })
+
+    let allAnswers = [...incorrectAnswers, <div key={props.correctAnswer} className="answer" onClick={event => props.selectAnswer(event)}>{props.correctAnswer}</div>]
+    
     return (
-        <div className="question-div">
-            <p>Question</p>
-            {ans}
+        <div className="questions-div">
+            <div className="answers-div">
+                    <h4>{props.question}</h4>
+                        {allAnswers}
+                    <button className="btn btn-success" onClick={props.handleClick}>Next</button>
+                </div>
         </div>
     )
 }
